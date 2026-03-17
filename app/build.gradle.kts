be6df316223d91fc79f66a5b3252ec0899705dd7
jasmine <jasmineirawan1@gmail.com>
2026-03-17 15:04:25 +1100

    Migrate to built-in kotlin and Add Supabase imports
    - Migration reason: feature deprecation, kotlin.android dependency import no longer required.
    
    - Add/Enable buildConfig.
    
    - Add Supabase dependencies.
    
    - Add Lifecycle and ViewModel dependencies.
    
    - Add Compose and Navigation dependencies.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,12 +1,26 @@
+// To allow for Auth set up
+// Exposes these dependencies to local.properties via BuildConfig.
+import java.util.Properties
+
+val localProperties = Properties()
+val localPropsFile = File(rootProject.rootDir, "local.properties")
+if (localPropsFile.exists() && localPropsFile.isFile) {
+    localPropsFile.inputStream().use {
+        localProperties.load(it)
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+//    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
 
     // For DB (Supabase) set up.
-    // Current Kotlin version used is based on libs.versions.toml file (08/03/2026).
-    kotlin("plugin.serialization") version "2.2.10" // Replace with actual version in project.
+    // Current Kotlin version used is based on libs.versions.toml file (17/03/2026).
+    // Serialization is for encoding and decoding custom objects. -Kotlin ref v3.0 supabase.com
+    kotlin("plugin.serialization") version "2.3.10" // Replace with actual version in project.
 }
+
 // IDE says the syntax is deprecated. Check for newer version (if documentation exists).
 android {
     namespace = "com.fit3161.fit3162.mogo"
@@ -20,6 +34,20 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // Supabase
+//        buildConfigField("String", "SUPABASE_URL",
+//            localProperties.getProperty("SUPABASE_URL")!!)
+
+        buildConfigField(
+            "String",
+            "SUPABASE_URL",
+            "\"${localProperties.getProperty("SUPABASE_URL", "")}\"")
+
+        buildConfigField(
+            "String",
+            "SUPABASE_ANON_KEY",
+            "\"${localProperties.getProperty("SUPABASE_ANON_KEY", "")}\"")
     }
 
     buildTypes {
@@ -35,11 +63,14 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions { // Also deprecated?
-        jvmTarget = "11"
-    }
+//    kotlinOptions { // Also deprecated?
+//        jvmTarget = "11"
+//    }
+
     buildFeatures {
+        buildConfig = true
         compose = true
+//        resValues = true
     }
 }
 
@@ -61,17 +92,31 @@ dependencies {
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
 
-    // For DB (Supabase) set up.
-    // Current version of Supabase and Ktor used as of 08/03/2026.
+    // Supabase
     implementation(platform("io.github.jan-tennert.supabase:bom:3.4.1"))
-    implementation("io.github.jan-tennert.supabase:postgrest-kt")
-    implementation("io.ktor:ktor-client-android:3.4.1")
+    implementation("io.github.jan-tennert.supabase:postgrest-kt") // PostgresSQL DB
+    implementation("io.github.jan-tennert.supabase:auth-kt") // DB authentication
+    implementation("io.github.jan-tennert.supabase:realtime-kt") // Realtime DB
+    implementation("io.ktor:ktor-client-android:3.4.1") // Ktor engine (required for Supabase Kotlin SDK)
+
 
     // KTX dependencies for coroutines (for LifecycleScope, ViewModelScope, liveData).
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+
+    // Lifecycle & ViewMdel
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0") // for kotlin coroutine??
 
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.10.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
+
+    // Compose
+    implementation("androidx.activity:activity-compose:1.9.0")
+    implementation(platform("androidx.compose:compose-bom:2024.09.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.material3:material3")
+
+    // Navigation
+    implementation("androidx.navigation:navigation-compose:2.7.7")
 
 }

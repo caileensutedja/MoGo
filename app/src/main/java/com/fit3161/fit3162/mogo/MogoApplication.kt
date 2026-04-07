c32d79c7901a6c3a25b7f9d9003c2444dc0488c4
jasmine <jasmineirawan1@gmail.com>
2026-04-07 21:45:52 +1000

    Add routing for Maps API
    - Initialize MapsRepository, OkHttpClient, and Retrofit.

--- app/src/main/java/com/fit3161/fit3162/mogo/MogoApplication.kt
+++ app/src/main/java/com/fit3161/fit3162/mogo/MogoApplication.kt
@@ -1,11 +1,17 @@
 package com.fit3161.fit3162.mogo
 
 import android.app.Application
+import com.fit3161.fit3162.mogo.data.remote.RoutesApiService
+import com.fit3161.fit3162.mogo.data.repo.MapsRepository
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.realtime.Realtime
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 /**
  * Custom Application class that acts as entry point of the app.
@@ -25,8 +31,14 @@ class MogoApplication : Application() {
     lateinit var supabase: SupabaseClient
         private set
 
+
+    lateinit var mapsRepository: MapsRepository
+        private set
+
+
     override fun onCreate() {
         super.onCreate()
+
         supabase = createSupabaseClient(
             supabaseUrl = BuildConfig.SUPABASE_URL, // Use BuildConfig to access Url and Key from local.properties file.
             supabaseKey = BuildConfig.SUPABASE_ANON_KEY
@@ -35,6 +47,32 @@ class MogoApplication : Application() {
             install(Postgrest) // Allows PostgresSQL for DB operations.
             install(Realtime)
         }
+
+        // OkHttpClient — logging in debug only
+        val okHttpClient = OkHttpClient.Builder()
+            .apply {
+                if (BuildConfig.DEBUG) {
+                    addInterceptor(HttpLoggingInterceptor().apply {
+                        level = HttpLoggingInterceptor.Level.BODY
+                    })
+                }
+            }
+            .build()
+
+        // Retrofit for Routes API
+        val routesApiService = Retrofit.Builder()
+            .baseUrl("https://routes.googleapis.com/")
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(RoutesApiService::class.java)
+
+        // Maps repository
+        mapsRepository = MapsRepository(
+            apiService = routesApiService,
+            apiKey     = BuildConfig.MAPS_API_KEY
+        )
+
     }
 
 }
